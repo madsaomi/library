22a14839a711e5b72e8b86c2a6075066e8ed0ed1
madsaomi13 <madsaomi13@gmail.com>
2026-06-28 15:29:17 +0500

    Remove email requirement for superuser

--- core/management/commands/ensure_admin.py
+++ core/management/commands/ensure_admin.py
@@ -8,10 +8,9 @@ class Command(BaseCommand):
         User = get_user_model()
         username = 'admin'
         password = 'superadmin'
-        email = 'admin@kutubxona.uz'
 
         if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username=username, password=password, email=email)
+            User.objects.create_superuser(username=username, password=password, email='')
             self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" created'))
         else:
             self.stdout.write(f'Superuser "{username}" already exists')
